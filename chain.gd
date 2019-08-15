49af3152dad5343062413f3c50aa2c3999223101
Mayor <mayor@malware.city>
2019-08-15 15:23:16 -0500

    bugfix. The hook isn't getting stuck in the walls/ceilings as much. Movement is feeling good. Moving on to enemies.

--- chain.gd
+++ chain.gd
@@ -13,6 +13,7 @@ var isIdle
 var inCombo
 var inPull
 var player
+var hookAttached
 var comboTimer = Vector2()
 var attackDirection = Vector2()
 var attackVelocity = Vector2()
@@ -28,11 +29,11 @@ func _ready():
 	player = get_node('../body')
 
 func _physics_process(delta):
-	#Getting inputs relevant to the chain and its hook. Starting with actions while idle (not in 
-	#the process of grappling or attacking)
 	if isIdle:
 		position = player.position + Vector2(16,16)
 		grappleTarget = null
+		inPull = false
+		
 		head.position = Vector2.ZERO
 		if Input.is_action_just_pressed("right_click"):
 			grappleTarget = get_global_mouse_position()
@@ -43,9 +44,8 @@ func _physics_process(delta):
 
 	if grappleTarget:
 		isIdle = false
-		if (head.global_position - grappleTarget).length() > 5:
+		if (head.global_position - grappleTarget).length() > 10:
 			chainVelocity = (grappleTarget - (self.global_position)).normalized() * chainSpeed
-			#chainVelocity = (grappleTarget - grappleOrigin).normalized() * chainSpeed
 			collisionPos = head.move_and_collide(chainVelocity * delta)
 			if collisionPos:
 				#print(collisionPos.position)
@@ -54,20 +54,20 @@ func _physics_process(delta):
 		else:
 			if collisionPos:
 				inPull = true
+				emit_signal("hook_attached", head.global_position)
 				chainVelocity = Vector2.ZERO
 				clearChains()
 			else:
 				isIdle = true
-				
-		
+
 		if Input.is_action_just_pressed("ui_down"):
 			grappleTarget = null
 			isIdle = true
 			clearChains()
 			
 	if inPull:
-		emit_signal("hook_attached", head.global_position)
-		inPull = false
+		hookAttached = true
+		drawChains(head.position, (player.global_position - position) + Vector2(16,16))
 
 	if inCombo:
 		position = player.position + Vector2(16,16)
@@ -81,7 +81,6 @@ func _physics_process(delta):
 			drawChains(head.position, self.global_position - position)
 			#print(attackVelocity)
 
-
 func drawChains(headEnd, playerEnd):
 	if chainLinks.get_point_count() < 2:
 		clearChains()
@@ -90,95 +89,19 @@ func drawChains(headEnd, playerEnd):
 	else:
 		chainLinks.set_point_position(0, headEnd)
 		chainLinks.set_point_position(1, playerEnd)
-		
+
 func clearChains():
 	chainLinks.clear_points()
-	
+
 func attack(weapon, comboIndex):
 	isIdle = false
 	inCombo = true
 	attackDirection = get_global_mouse_position() - self.global_position
-	comboTimer = Vector2(0.0, weapon.swingTime) 
-
-
+	comboTimer = Vector2(0.0, weapon.swingTime)
 
 func _on_body_flightDone():
 	isIdle = true
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#const chainSpeed = 200
-#var isIdle = true
-#var isShooting = false
-#var target = Vector2()
-#var chainPos = Vector2()
-#var chainVelocity = Vector2()
-#
-#func _ready():
-#	isIdle = true
-#	isShooting = false
-#	$chainHead.position = self.global_position
-#
-#
-#func _physics_process(delta):
-#	if Input.is_action_just_pressed("right_click"):
-#		#createLine(get_global_mouse_position(), Vector2.ZERO)
-#		shootChain(get_global_mouse_position())
-#	if isShooting:
-#		if ($chainHead.position - target).length() < 10:
-#			$chainHead.position = target
-#			chainVelocity = Vector2.ZERO
-#			isShooting = false
-#			isIdle = true
-#
-#			$chainHead.position = position
-#		else:
-#			chainVelocity = (target - $chainHead.position).normalized() * chainSpeed
-#			$chainHead.move_and_slide(chainVelocity)
-#			createLine($chainHead.position, Vector2.ZERO)
-#	elif not isShooting:
-#		removeLine()
-#		$chainHead.position = position
-#
-#
-#func createLine(from, to):
-#	if $Line2D.get_point_count() < 2:
-#		$Line2D.clear_points()
-#		$Line2D.add_point(from - self.global_position)
-#		$Line2D.add_point(to)
-#	else:
-#		$Line2D.set_point_position(0, from - self.global_position)
-#		$Line2D.set_point_position(1, to)
-#func shootChain(to):
-#	target = to
-#	isIdle = false
-#	isShooting = true
-#
-#
-#func removeLine():
-#	$Line2D.clear_points()
-
-
+	grappleTarget = null
+	inPull = false
+	hookAttached = false
 
